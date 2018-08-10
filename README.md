ffea6b62aa12b426b61e4bcfaf3a588d23f5d305
flyyang <pengfei_yang@outlook.com>
2018-08-11 00:14:52 +0800

    first version

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # Clean After Emit Webpack Plugin
 
-A webpack plugin used for after building.
-This can be used with johnagan's [clean-webpack-plugin](https://github.com/johnagan/clean-webpack-plugin).
+A webpack plugin used for cleaning after building.
+
+This can be used with johnagan's [clean-webpack-plugin](https://github.com/johnagan/clean-webpack-plugin) which clean files before building.
+
+Only works webpack 4.x+.
 
 ## Installation
 
-npm i  -D clean-after-emit-webpack-plugin
+```
+npm i -D clean-after-emit-webpack-plugin
+```
 
 ## Usage
 
+For now, only support one params. The `paths` absolute path array.
 
+```
+
+new CleanAfterEmitWebpackPlugin({
+  paths: [absPath, absPath],
+})
+```
+
+New Features and Pr are welcomed.
--- index.js
+++ index.js
@@ -1,5 +1,42 @@
+const path = require('path');
+const weblog = require('webpack-log');
+const rimraf = require('rimraf');
 const version = require('./package.json').version;
 
-module.exports = {
-  version,
+const PluginName = 'Clean After Emit Webpack Plguin';
+const log = weblog({name: PluginName});
+
+function isAbsolutePath (userPath) {
+  return path.isAbsolute(userPath);
 }
+
+function rm(paths){
+  if (!Array.isArray(paths)) return;
+
+  paths.forEach(p => {
+    if (!isAbsolutePath(p)) return;
+    rimraf.sync(p);
+    log.info(`remove ${p} after build`);
+  })
+}
+
+module.exports = class CleanAfterEmitWebpackPlguin {
+  constructor({
+    paths = [],
+  }) {
+    this.paths = paths;
+    this.version = version;
+    this.PluginName = PluginName;
+  }
+
+  apply(compiler) {
+    console.log(this.PluginName);
+    compiler.hooks.afterEmit.tapAsync(
+      this.PluginName,
+      (compilation, cb) => {
+        rm(this.paths);
+        cb();
+      }
+    )
+  }
+};
